ee9811393b83187d1f492dbd3c110b4eadea0580
Torstein Eide <torse@ntnu.no>
2021-10-15 09:08:44 +0200

    +Added java.Logging to module-info.java

--- ChatClient/src/main/java/module-info.java
+++ ChatClient/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module no.ntnu.datakomm.chat {
   requires javafx.controls;
   requires javafx.fxml;
+  requires java.logging;
 
   opens no.ntnu.datakomm.chat to javafx.fxml;
   exports no.ntnu.datakomm.chat;
